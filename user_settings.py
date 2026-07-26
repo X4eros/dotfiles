a4ecb9ac7af67017d3ba7b509e7fda36fcc698f9
X4eros <by_yakup_10@hotmail.com>
2026-07-26 16:57:42 +0300

    DMS

---
+++ user_settings.py
@@ -0,0 +1,16 @@
+#To enable these settings, name this file "user_settings.py".
+
+#Settings here will take effect for all games run in this Proton version.
+
+user_settings = {
+    "PROTON_DLSS_UPGRADE": "1",
+    "PROTON_DLSS_INDICATOR": "0",
+    "PROTON_ENABLE_NGX_UPDATER": "1",
+    "DXVK_NVAPI_DRS_NGX_DLSS_SR_OVERRIDE": "on",
+    "DXVK_NVAPI_DRS_NGX_DLSS_RR_OVERRIDE": "on",
+    "DXVK_NVAPI_DRS_NGX_DLSS_FG_OVERRIDE": "on",
+    "DXVK_NVAPI_DRS_NGX_DLSS_SR_OVERRIDE_RENDER_PRESET_SELECTION": "RENDER_PRESET_K",
+    "DXVK_NVAPI_DRS_NGX_DLSS_RR_OVERRIDE_RENDER_PRESET_SELECTION": "RENDER_PRESET_K",
+    "PROTON_VKD3D_LOWLATENCY": "1",
+    "PROTON_DISCORD_BRIDGE" : "1",
+}
