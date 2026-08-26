b964b56a37a7bf3826dc5b1e573a7685819ca121
X4eros <by_yakup_10@hotmail.com>
2026-08-26 18:11:28 +0300

    DMS

--- thunar/.config/Thunar/accels.scm
+++ thunar/.config/Thunar/accels.scm
@@ -4,6 +4,7 @@
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-type" "")
 ; (gtk_accel_path "<Actions>/ThunarStatusBar/toggle-last-modified" "")
 ; (gtk_accel_path "<Actions>/Thunarwindow/menu" "")
+; (gtk_accel_path "<Actions>/ThunarBookmarks/8e2cc446f8392ea853b7bc1ae96ac90d" "")
 ; (gtk_accel_path "<Actions>/ThunarActionManager/cut" "<Primary>x")
 ; (gtk_accel_path "<Actions>/ThunarStandardView/sort-by-size" "")
 ; (gtk_accel_path "<Actions>/ThunarWindow/file-menu" "")
